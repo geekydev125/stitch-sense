a3e2146de96233cd8956ea4b7d2ef194277c1e33
geekydev125 <goldenstardev1108@gmail.com>
2024-01-09 18:47:40 +0200

    Creating the arrow animation in the Hero Section

--- client/src/components/Sections/HeroSection.tsx
+++ client/src/components/Sections/HeroSection.tsx
@@ -1,6 +1,7 @@
 import { useRef } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
-import stitchingVideo from '/assets/video/stitching-hd.mp4'
+import stitchingVideo from '/assets/video/stitching-hd-black-white.mp4'
 import paperPatternTransparent from '/assets/img/paper-pattern-with-transparency.png'
 import blackLogo from '/assets/img/stitchSense-black-logo.png'
 
@@ -10,6 +11,8 @@ import Stack from '@mui/system/Stack';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 
+import ArrowCircleDownIcon from '@mui/icons-material/ArrowCircleDown';
+import { IconButton } from '@mui/material';
 
 function HeroSection() {
     const videoRef = useRef<null | HTMLVideoElement>(null);
@@ -19,8 +22,19 @@ function HeroSection() {
             videoRef.current.play();
         }
     };
+
+    const movingAnimation = {
+        bottom: ["8%", "6%"], // Keyframes for pulsating effect
+        transition: {
+            duration: 1.5, // Total duration for one pulsating cycle
+            repeat: Infinity, // Infinite animation loop
+        },
+    };
+
+
     return (
         <>
+
             <Box
                 sx={{
                     width: '100%',
@@ -38,12 +52,30 @@ function HeroSection() {
                         <Stack width='50%'>
                             <Typography variant='h1' component='h1'>StitchSense</Typography>
                             <Typography variant='h6' component='h3'>WHERE YOUR BUSINESS PRODUCTION NEEDS ARE MET</Typography>
-                            <img style={{alignSelf: 'center', marginTop: '30px'}} src={blackLogo} width='150px' alt='Black Logo' />
+                            <img style={{ alignSelf: 'center', marginTop: '30px' }} src={blackLogo} width='150px' alt='Black Logo' />
                         </Stack>
                     </Stack>
                 </Container>
-            </Box>
 
+                <AnimatePresence>
+                    <motion.div
+                        // initial={{ bottom: '5%',transform: 'translate(-50%, -5%)' }}
+                        animate={movingAnimation}
+                        // exit={{ bottom: '2%',transform: 'translate(-50%, -2%)' }}
+                        style={{
+                            position: 'absolute',
+                            zIndex: 10,
+                            bottom: '8%',
+                            left: '50%',
+                            transform: 'translate(-50%, -8%)'
+                        }}
+                    >
+                        <IconButton>
+                            <ArrowCircleDownIcon sx={{fontSize: 45}} />
+                        </IconButton>
+                    </motion.div>
+                </AnimatePresence>
+            </Box>
 
 
             <Stack direction='row' justifyContent='flex-end'>
