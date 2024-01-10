8459030cb0f5f5a04f1285ca5bed9bcbaa7011d8
geekydev125 <goldenstardev1108@gmail.com>
2024-01-10 17:50:53 +0200

    Addition of the NumbersSection to the HomeView

--- client/src/views/HomeView.tsx
+++ client/src/views/HomeView.tsx
@@ -3,6 +3,7 @@ import Header from "../components/Header/Header"
 import AboutSection from "../components/Sections/AboutSection"
 import HeroSection from "../components/Sections/HeroSection"
 import WhatWeDoSection from "../components/Sections/WhatWeDoSection"
+import NumbersSection from "../components/Sections/NumbersSection"
 
 function HomeView() {
 	return (
@@ -13,7 +14,9 @@ function HomeView() {
 			<Container sx={{ padding: '30px 0' }}>
 				<AboutSection />
 				<WhatWeDoSection />
+				
 			</Container>
+			<NumbersSection />
 		</>
 	)
 }
