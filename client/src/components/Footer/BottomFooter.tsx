726241469d85e506d90cda1be825f62b49b91018
geekydev125 <goldenstardev1108@gmail.com>
2024-01-11 07:22:00 +0200

    Creating a Footer component, which will contain the top and bottom footers

--- client/src/components/Footer/BottomFooter.tsx
+++ client/src/components/Footer/BottomFooter.tsx
@@ -12,14 +12,16 @@ function BottomFooter() {
             sx={{
                 width: '100%',
                 height: '70px',
-                backgroundColor: 'custom.theme.lightGreen'
+                backgroundColor: 'custom.theme.lightGreen',
+                position: 'sticky',
+                bottom: 0
             }}
         >
             <Container sx={{ height: '100%' }}>
                 <Stack justifyContent='center' alignItems='center' sx={{ height: '100%' }}>
-                    <Typography fontWeight='bold'>
+                    <Typography fontWeight='bold' color='white'>
                         Powered by&nbsp; 
-                        <Link href="http://www.arielbehar.com" target="_blank" sx={{textDecoration: 'none'}}>
+                        <Link href="http://www.arielbehar.com" target="_blank" sx={{textDecoration: 'none'}} style={{ color: 'gray'}}>
                             Ariel Behar
                             <Box component='span' position='relative' bottom='-7px'>
                                 <Box sx={{height: '25px'}} mx={1} component='img' src={arielbeharLogo} alt='Ariel Behar Logo' />
---
+++ client/src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import Box from "@mui/material/Box"
+import BottomFooter from "./BottomFooter"
+
+function Footer() {
+	return (
+		<Box component="footer">
+
+			<BottomFooter />
+		</Box>
+    
+  )
+}
+
+export default Footer
