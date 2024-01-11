cc8d0a4b5ba8b64e8e8e206582f63eb61c34a5cf
geekydev125 <goldenstardev1108@gmail.com>
2024-01-11 06:50:19 +0200

    Implementation of the BottomFooter component

---
+++ client/src/components/Footer/BottomFooter.tsx
@@ -0,0 +1,36 @@
+import arielbeharLogo from '/assets/img/arielbehar-logo.png'
+
+import Box from "@mui/material/Box"
+import Container from "@mui/material/Container"
+import Link from "@mui/material/Link"
+import Stack from "@mui/material/Stack"
+import Typography from "@mui/material/Typography"
+
+function BottomFooter() {
+    return (
+        <Box
+            sx={{
+                width: '100%',
+                height: '70px',
+                backgroundColor: 'custom.theme.lightGreen'
+            }}
+        >
+            <Container sx={{ height: '100%' }}>
+                <Stack justifyContent='center' alignItems='center' sx={{ height: '100%' }}>
+                    <Typography fontWeight='bold'>
+                        Powered by&nbsp; 
+                        <Link href="http://www.arielbehar.com" target="_blank" sx={{textDecoration: 'none'}}>
+                            Ariel Behar
+                            <Box component='span' position='relative' bottom='-7px'>
+                                <Box sx={{height: '25px'}} mx={1} component='img' src={arielbeharLogo} alt='Ariel Behar Logo' />
+                            </Box>
+                        </Link>
+                    </Typography>
+                </Stack>
+
+            </Container>
+        </Box>
+    )
+}
+
+export default BottomFooter
