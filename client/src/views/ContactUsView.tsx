9ad075327c52475c80d68e6477d0b31da0405b57
geekydev125 <goldenstardev1108@gmail.com>
2024-01-11 16:48:22 +0200

    creation of the contact form

--- client/src/views/ContactUsView.tsx
+++ client/src/views/ContactUsView.tsx
@@ -1,8 +1,45 @@
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+import contactFormSchema from '../validations/contactFormSchema';
+
+
+interface FormData {
+	firstName: string,
+	lastName: string,
+	phone: string,
+	email: string,
+	message: string
+}
 
 function ContactUsView() {
-  return (
-    <div>ContactUsView</div>
-  )
+	const { register, handleSubmit, formState: { errors, isDirty, isValid }} = useForm<FormData>({
+		mode: 'onBlur',
+		resolver: yupResolver(contactFormSchema),
+		defaultValues: {
+			firstName: '',
+			lastName: '',
+			email: '',
+			phone: '',
+			message: ''
+		}
+
+	})
+
+	const onFormSubmit = () => {
+		console.log('submit');
+	}
+
+	return (
+		<>
+		<form onSubmit={handleSubmit(onFormSubmit)}>
+
+
+			
+		</form>
+		
+		</>
+	)
 }
 
 export default ContactUsView
