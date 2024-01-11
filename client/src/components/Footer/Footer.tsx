6b61e4bb6a290b675d8e027265fb40d5f577b58e
geekydev125 <goldenstardev1108@gmail.com>
2024-01-11 16:05:14 +0200

    Implementation of the TopFooter (without Google Map)

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,10 +1,11 @@
 import Box from "@mui/material/Box"
 import BottomFooter from "./BottomFooter"
+import TopFooter from "./TopFooter"
 
 function Footer() {
 	return (
 		<Box component="footer">
-
+			<TopFooter />
 			<BottomFooter />
 		</Box>
     
---
+++ client/src/components/Footer/TopFooter.tsx
@@ -0,0 +1,42 @@
+import googleMapPlaceHolder from '/assets/img/google-map.png'
+
+import Box from "@mui/material/Box"
+import Container from "@mui/material/Container"
+import Grid from "@mui/material/Grid"
+import Stack from "@mui/material/Stack"
+import Typography from "@mui/material/Typography"
+
+function TopFooter() {
+    return (
+        <Box sx={{ backgroundColor: 'black' }} py={3}>
+            <Container>
+                <Grid container>
+                    <Grid item xs={12} md={4}>
+                        <Stack height='100%' spacing={2} padding={2} flexDirection='column' justifyContent='space-around'>
+                            <Box>
+                                <Typography variant='h5' component='h5' color='white' fontWeight='bold'>Contact Details:</Typography>
+                                <Typography variant='body1' component='p' color='white'>Name: Ganimir Vangelov</Typography>
+                                <Typography variant='body1' component='p' color='white'>Phone: +(359) 888 81 11 32</Typography>
+                                <Typography variant='body1' component='p' color='white'>Email: ganimirvangelov@gmail.com</Typography>
+                            </Box>
+                            <Box>
+                                <Typography variant='h5' component='h5' color='white' fontWeight='bold'>Location:</Typography>
+                                <Typography variant='body1' component='p' color='white'>280 Tsarigradski Complex,</Typography>
+                                <Typography variant='body1' component='p' color='white'>Sofia, Bulgaria</Typography>
+                            </Box>
+                        </Stack>
+                    </Grid>
+                    <Grid item xs={12} md={8}>
+                        <Stack flexDirection='column' justifyContent='center' p={1} height='100%'>
+                            <img src={googleMapPlaceHolder} alt='Google Map Placeholder' />
+                        </Stack>
+                    </Grid>
+                </Grid>
+            </Container>
+
+
+        </Box>
+    )
+}
+
+export default TopFooter
