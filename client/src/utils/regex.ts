7ea44a87fdaada9f029338c0b4d3ae3cb1ddd166
geekydev125 <goldenstardev1108@gmail.com>
2024-01-11 16:47:13 +0200

    Creation of the regex file for validations

---
+++ client/src/utils/regex.ts
@@ -0,0 +1,4 @@
+//Valid Phone number pattern:
+export const PHONE_PATTERN_REGEX = /^(\+?\d{0,4})?\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{4}\)?)?$/
+
+export const LATIN_CHARACTERS_REGEX = /[a-zA-z]/i
