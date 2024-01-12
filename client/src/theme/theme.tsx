a47c5810d0c6810b9588f9badbfe6f4d889fc0bd
geekydev125 <goldenstardev1108@gmail.com>
2024-01-12 07:07:41 +0200

    Making fonts responsive on all screen sizes

--- client/src/theme/theme.tsx
+++ client/src/theme/theme.tsx
@@ -1,6 +1,6 @@
-import { createTheme } from "@mui/material"
+import { createTheme, responsiveFontSizes } from "@mui/material"
 
-const theme = createTheme({
+let theme = createTheme({
     palette: {
         custom: {
             theme: {
@@ -15,5 +15,7 @@ const theme = createTheme({
         }
     }
 })
+
+theme = responsiveFontSizes(theme);
 
 export default theme;
