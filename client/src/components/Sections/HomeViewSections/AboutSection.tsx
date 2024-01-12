8c5238986c03f9df122bd66542c4db6cb6b097ec
geekydev125 <goldenstardev1108@gmail.com>
2024-01-12 06:56:16 +0200

    Optimizing padding of the AboutSection elements for mobile

--- client/src/components/Sections/HomeViewSections/AboutSection.tsx
+++ client/src/components/Sections/HomeViewSections/AboutSection.tsx
@@ -14,12 +14,13 @@ function AboutSection() {
 						backgroundSize: 'contain',
 						backgroundPosition: 'left center'
 					}}
+					p={2}
 				>
-					<Typography variant='h5' component='p' mb={5} pl={25} pt={2} textAlign='justify'>
+					<Typography variant='h5' component='p' mb={5} pl={{md: 25}} textAlign='justify'>
 						We are a clothing manufacturer with more than 10 years of experience in the fashion industry, <b>StitchSense</b> specializes in sublimation on polyesters and a wide array of sewing services, with an emphasis on t-shirts, hoodies, sweaters, sportswear, loungewear, and costumes/jumpsuits. We pride ourselves in providing high-quality apparel products and speedy, customer-centric service.
 					</Typography>
 
-					<Typography variant='h5' component='p' pl={25} pb={2} textAlign='justify'>
+					<Typography variant='h5' component='p' pl={{md: 25}} pb={2} textAlign='justify'>
 						We are European-based and have our own successful clothing brands like <b>CandyCatz</b>, <b>Badinka</b>, and <b>RaveKitties</b>, all of which have been creating fashion for our satisfied customers for more than 10 years.
 					</Typography>
 				</Box>
