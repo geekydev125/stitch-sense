430ccdc21431ecbedb5e71501e566a1034e722bb
geekydev125 <goldenstardev1108@gmail.com>
2024-01-13 07:35:09 +0200

    Adding an additional title to the TopFooter

--- client/src/components/Footer/TopFooter.tsx
+++ client/src/components/Footer/TopFooter.tsx
@@ -14,6 +14,9 @@ function TopFooter() {
                     <Grid item xs={12} md={4}>
                         <Stack height='100%' spacing={2} padding={2} flexDirection='column' justifyContent='space-around'>
                             <Box>
+                                <Typography variant='h4' component='h5' color='custom.theme.lightGreen'>LET’S WORK TOGETHER!</Typography>
+                            </Box>
+                            <Box>
                                 <Typography variant='h5' component='h5' color='white' fontWeight='bold'>Contact Details:</Typography>
                                 <Typography variant='body1' component='p' color='white'>Name: Ganimir Vangelov</Typography>
                                 <Typography variant='body1' component='p' color='white'>Phone: +(359) 888 81 11 32</Typography>
