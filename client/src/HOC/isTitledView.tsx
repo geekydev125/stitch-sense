b2fa99f4d88bc777ef091c4ee72bb7076551d8fc
geekydev125 <goldenstardev1108@gmail.com>
2024-01-14 14:30:27 +0200

    Creation of the isTitledView HOC and removal of the backgrounds from RootView

---
+++ client/src/HOC/isTitledView.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import ViewTopTitleSection from '../components/ViewTopTitleSection';
+
+import wavyLinesTop from '../assets/img/wavy-lines-top.png'
+import wavyLinesBottom from '../assets/img/wavy-lines-bottom.png'
+
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+
+interface Props {
+    viewTitle: string,
+    imageSrc: string,
+    backgroundPosition: string,
+    backgroundSize?: string
+}
+
+function isTitleView<T>(WrappedComponent: React.ComponentType<T>, { viewTitle, imageSrc, backgroundPosition, backgroundSize }: Props) {
+    return (props: T) => (
+        <>
+        
+            <ViewTopTitleSection viewTitle={viewTitle} imageSrc={imageSrc} backgroundPosition={backgroundPosition} backgroundSize={backgroundSize} />
+            <Stack component='main' flexGrow={1}
+                sx={{
+                    background: `url(${wavyLinesTop})`,
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'contain',
+                    backgroundPosition: 'top right'
+                }}
+            >
+                <Box
+                    sx={{
+                        background: `url(${wavyLinesBottom})`,
+                        backgroundRepeat: 'no-repeat',
+                        backgroundSize: 'contain',
+                        backgroundPosition: 'bottom left'
+                    }}
+                >
+                    <WrappedComponent {...props as any} />
+                </Box>
+            </Stack>
+        </>
+    );
+}
+
+export default isTitleView;
--- client/src/views/RootView.tsx
+++ client/src/views/RootView.tsx
@@ -1,12 +1,7 @@
 import { Outlet, useLocation } from "react-router-dom";
 
-import wavyLinesTop from '../assets/img/wavy-lines-top.png'
-import wavyLinesBottom from '../assets/img/wavy-lines-bottom.png'
-
 import Header from "../components/Header/Header";
-import Stack from "@mui/material/Stack";
 import Footer from "../components/Footer/Footer";
-import Box from "@mui/material/Box";
 
 function RootView() {
 	const location = useLocation();
@@ -17,25 +12,7 @@ function RootView() {
 		<>
 			{!isHomeView && <Header />}
 
-			<Stack component='main' flexGrow={1}
-				sx={!isHomeView ? {
-					background: `url(${wavyLinesTop})`,
-					backgroundRepeat: 'no-repeat',
-					backgroundSize: 'contain',
-					backgroundPosition: 'top right'
-				} : {}}
-			>
-				<Box
-					sx={{
-						background: `url(${wavyLinesBottom})`,
-						backgroundRepeat: 'no-repeat',
-						backgroundSize: 'contain',
-						backgroundPosition: 'bottom left'
-					}}
-				>
-					<Outlet/>
-				</Box>
-			</Stack>
+			<Outlet />
 
 			<Footer />
 		</>
