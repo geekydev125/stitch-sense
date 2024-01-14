2208a96b66b1dc9df33471b16744544a737679b6
geekydev125 <goldenstardev1108@gmail.com>
2024-01-14 14:55:03 +0200

    Creating a basic ErrorView (not finished)

--- client/src/views/ErrorView.tsx
+++ client/src/views/ErrorView.tsx
@@ -1,7 +1,24 @@
+import Stack from "@mui/material/Stack";
+import Container from "@mui/material/Container";
+
+import Footer from "../components/Footer/Footer";
+import Header from "../components/Header/Header";
+
 function ErrorView() {
-  return (
-    <div>ErrorView</div>
-  )
+
+	return (
+		<Stack minHeight={'100vh'}>
+			<Header />
+
+			<Container style={{ flexGrow: 1 }}>
+				<h2>ErrorView</h2>
+				<h1>Under Construction</h1>
+				<h2>ErrorView</h2>
+			</Container>
+
+			<Footer />
+		</Stack>
+	)
 }
 
 export default ErrorView
