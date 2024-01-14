b45c9147e0ee71d0332cc89d65f452840d46b372
geekydev125 <goldenstardev1108@gmail.com>
2024-01-14 18:52:51 +0200

    Creating the Loader component and adding in it the logo and BouncingDotsLoader

---
+++ client/src/components/Loader.tsx
@@ -0,0 +1,31 @@
+
+import stitchSenseLogo from '../assets/img/stitchSense-logo.png'
+import Stack from "@mui/material/Stack"
+import BouncingDotsLoader from "./BouncingDotsLoader"
+import Box from '@mui/material/Box'
+
+function Loader() {
+
+
+    return (
+        <Stack
+            justifyContent="center"
+            alignItems="center"
+            zIndex={10}
+
+            sx={{
+                position: 'absolute',
+                width: '100vw',
+                height: '100vh',
+                backgroundColor: 'custom.theme.lightGreen',
+            }}
+        >
+            <img src={stitchSenseLogo} alt="Stitch Sense Logo" />
+            <Box mt={5}>
+                <BouncingDotsLoader />
+            </Box>
+        </Stack>
+    )
+}
+
+export default Loader
