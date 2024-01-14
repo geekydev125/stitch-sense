c377280750a3387285abbd3592bff410ec84b19d
geekydev125 <goldenstardev1108@gmail.com>
2024-01-14 08:07:05 +0200

    Aligning the CTA button to the bottom of the grid container

--- client/src/components/ServiceTabs/ServiceDescriptionColumn.tsx
+++ client/src/components/ServiceTabs/ServiceDescriptionColumn.tsx
@@ -1,35 +1,40 @@
+import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
+import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
 
 function ServiceDescriptionColumn() {
     return (
         <>
-            <Typography variant='body1' component='p'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam nihil facilis dolores deleniti iure at perferendis dolor nobis culpa natus vel ipsa minus dicta possimus, vero iusto ducimus aut nisi ratione dignissimos, aperiam repellat ullam? Reiciendis, eveniet earum placeat, cumque laboriosam quo nihil incidunt minima nostrum in minus itaque esse.
-            </Typography>
-            <br />
-            <Typography variant='body1' component='p'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam nihil facilis dolores deleniti iure at perferendis dolor nobis culpa natus vel ipsa minus dicta possimus, vero iusto ducimus aut nisi ratione dignissimos, aperiam repellat ullam? Reiciendis, eveniet earum placeat, cumque laboriosam quo nihil incidunt minima nostrum in minus itaque esse.
-            </Typography>
-            <br />
-            <Typography variant='body1' component='p'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam nihil facilis dolores deleniti iure at perferendis dolor nobis culpa natus vel ipsa minus dicta possimus, vero.
-            </Typography>
+            <Stack minHeight='620px'>
+                <Box mb={2}>
+                    <Typography variant='body1' component='p'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam nihil facilis dolores deleniti iure at perferendis dolor nobis culpa natus vel ipsa minus dicta possimus, vero iusto ducimus aut nisi ratione dignissimos, aperiam repellat ullam? Reiciendis, eveniet earum placeat, cumque laboriosam quo nihil incidunt minima nostrum in minus itaque esse.
+                    </Typography>
+                    <br />
+                    <Typography variant='body1' component='p'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam nihil facilis dolores deleniti iure at perferendis dolor nobis culpa natus vel ipsa minus dicta possimus, vero iusto ducimus aut nisi ratione dignissimos, aperiam repellat ullam? Reiciendis, eveniet earum placeat, cumque laboriosam quo nihil incidunt minima nostrum in minus itaque esse.
+                    </Typography>
+                    <br />
+                    <Typography variant='body1' component='p'>
+                        Lorem ipsum Lorem vel ipsa minus dicta possimus, vero.
+                    </Typography>
+                </Box>
 
-            <Button
-                variant='contained'
-                size='large'
-                sx={{
-                    margin: '20px auto 0px auto',
-                    display: 'block',
-                    backgroundColor: 'custom.theme.lightGreen',
-                    "&:hover": {
-                        backgroundColor: 'custom.theme.darkGreen',
-                    }
-                }}
-            >
-                GET A QUOTE
-            </Button>
+                <Button
+                    variant='contained'
+                    size='large'
+                    sx={{
+                        margin: 'auto auto 20px auto',
+                        backgroundColor: 'custom.theme.lightGreen',
+                        "&:hover": {
+                            backgroundColor: 'custom.theme.darkGreen',
+                        }
+                    }}
+                >
+                    GET A QUOTE
+                </Button>
+            </Stack>
         </>
     )
 }
