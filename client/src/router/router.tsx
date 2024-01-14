3d0c6207fc7f073310836290e528c0883df8418c
geekydev125 <goldenstardev1108@gmail.com>
2024-01-14 18:53:33 +0200

    Adding lazy loading and the Loader component to all views

--- client/src/router/router.tsx
+++ client/src/router/router.tsx
@@ -1,43 +1,69 @@
+import { Suspense, lazy } from 'react';
 import { createBrowserRouter } from 'react-router-dom'
 
-import RootView from '../views/RootView';
+// import RootView from '../views/RootView';
 import ErrorView from '../views/ErrorView';
-import HomeView from '../views/HomeView';
-import AboutView from '../views/AboutView';
-import OurProcessView from '../views/OurProcessView';
-import PortfolioView from '../views/PortfolioView';
-import ContactUsView from '../views/ContactUsView';
-import ServicesView from '../views/ServicesView';
+// import HomeView from '../views/HomeView';
+// import AboutView from '../views/AboutView';
+// import OurProcessView from '../views/OurProcessView';
+// import PortfolioView from '../views/PortfolioView';
+// import ContactUsView from '../views/ContactUsView';
+// import ServicesView from '../views/ServicesView';
+
+import Loader from '../components/Loader';
+
+const LazyRootView = lazy(() => import('../views/RootView'));
+
+const LazyHomeView = lazy(() => import('../views/HomeView'));
+const LazyAboutView = lazy(() => import('../views/AboutView'));
+const LazyOurProcessView = lazy(() => import('../views/OurProcessView'));
+const LazyPortfolioView = lazy(() => import('../views/PortfolioView'));
+const LazyContactUsView = lazy(() => import('../views/ContactUsView'));
+const LazyServicesView = lazy(() => import('../views/ServicesView'));
 
 const router = createBrowserRouter([
 	{
 		path: "/",
-		element: <RootView />,
+		element: <Suspense fallback={<Loader />}>
+			<LazyRootView />
+		</Suspense>,
 		errorElement: <ErrorView />,
 		children: [
 			{
 				index: true,
-				element: <HomeView />
+				element: <Suspense fallback={<Loader />}>
+					<LazyHomeView />
+				</Suspense>
 			},
 			{
 				path: "/services",
-				element: <ServicesView />
+				element: <Suspense fallback={<Loader />}>
+					<LazyServicesView />
+				</Suspense>
 			},
 			{
 				path: "/our-process",
-				element: <OurProcessView />
+				element: <Suspense fallback={<Loader />}>
+					<LazyOurProcessView />
+				</Suspense>
 			},
 			{
 				path: "/portfolio",
-				element: <PortfolioView />
+				element: <Suspense fallback={<Loader />}>
+					<LazyPortfolioView />
+				</Suspense>
 			},
 			{
 				path: "/about-us",
-				element: <AboutView />
+				element: <Suspense fallback={<Loader />}>
+					<LazyAboutView />
+				</Suspense>
 			},
 			{
 				path: "/contact-us",
-				element: <ContactUsView />
+				element: <Suspense fallback={<Loader />}>
+					<LazyContactUsView />
+				</Suspense>
 			},
 		]
 	}
