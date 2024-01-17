f92e1f618e49f4b2988890622a8f8ef03966e411
geekydev125 <goldenstardev1108@gmail.com>
2024-01-17 07:37:39 +0200

    updating the content of the About Us and WhatWeDo sections

--- client/src/components/Sections/HomeViewSections/AboutSection.tsx
+++ client/src/components/Sections/HomeViewSections/AboutSection.tsx
@@ -19,14 +19,7 @@ function AboutSection({
 		<>
 			<Box ref={aboutSectionRef}>
 				<SectionTitle title='ABOUT US' mt={4} mb={0} p={2} />
-				<Box
-					sx={{
-						borderLeft: '1px solid lightgray',
-						borderRight: '1px solid lightgray',
-						borderBottom: '1px solid lightgray',
-					}}
-					p={3}
-				>
+				<Box p={3}>
 					<Box
 						sx={{
 							background: `url(${euFlag}) no-repeat`,
@@ -34,15 +27,15 @@ function AboutSection({
 							backgroundPosition: 'left center',
 						}}
 					>
-						<Typography variant='h5' component='p' mb={5} pl={{ md: 25 }} textAlign='justify' >
-							We are a clothing manufacturer with more than 10 years of experience in the fashion industry. <b>StitchSense</b> specializes in sublimation on polyesters and a wide array of sewing services, with an emphasis on t-shirts, hoodies, sweaters, sportswear, loungewear, and costumes/jumpsuits. We pride ourselves in providing high-quality apparel products and speedy, customer-centric service.
+						<Typography variant='body1' component='p' mb={5} pl={{ md: 25 }} textAlign='justify' >
+							We are a clothing manufacturer with more than 10 years of experience in the fashion industry. 
 						</Typography>
 
-						<Typography variant='h5' component='p' mb={5} pl={{ md: 25 }} textAlign='justify'>
+						<Typography variant='body1' component='p' mb={5} pl={{ md: 25 }} textAlign='justify'>
 							As a white-label provider, we cater to diverse retail levels, from mass to specialty, department stores, off-price retailers, and e-commerce platforms. Our emphasis lies in swift market delivery and direct-to-consumer approaches.
 						</Typography>
 
-						<Typography variant='h5' component='p' pl={{ md: 25 }} pb={2} textAlign='justify'>
+						<Typography variant='body1' component='p' pl={{ md: 25 }} pb={2} textAlign='justify'>
 							We are European-based and have our own successful clothing brands like <b>CandyCatz</b>, <b>Badinka</b>, and <b>RaveKitties</b>, all of which have been creating fashion for our satisfied customers for more than 10 years.
 						</Typography>
 					</Box>
--- client/src/components/Sections/HomeViewSections/WhatWeDoSection.tsx
+++ client/src/components/Sections/HomeViewSections/WhatWeDoSection.tsx
@@ -5,11 +5,16 @@ import FlipCard from '../../FlipCard';
 import SectionTitle from '../../SectionTitle'
 
 import Grid from '@mui/material/Grid';
+import { Typography } from '@mui/material';
 
 function WhatWeDoSection() {
 	return (
 		<>
 			<SectionTitle title='WHAT WE DO' mt={4} mb={2} p={2} />
+
+			<Typography variant='body1' component='p' textAlign='center' mt={2} mb={4}>
+				<b>StitchSense</b> specializes in sublimation on polyesters and a wide array of sewing services, with an emphasis on t-shirts, hoodies, sweaters, sportswear, loungewear, and costumes/jumpsuits. We pride ourselves in providing high-quality apparel products and speedy, customer-centric service.
+			</Typography>
 
 			<Grid container spacing={2}>
 
