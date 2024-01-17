1e9cbdc5730de34d872b8ea1cf287b533c1d3287
geekydev125 <goldenstardev1108@gmail.com>
2024-01-17 18:24:58 +0200

    Implementation of the EmployeeCard's structure, content and styles

--- client/src/components/Cards/EmployeeCard.tsx
+++ client/src/components/Cards/EmployeeCard.tsx
@@ -1,14 +1,41 @@
-interface Props extends IemployeeCard {}
+import Box from "@mui/material/Box"
+import { IemployeeCard } from "../../views/AboutView"
+import { Typography } from "@mui/material"
+
+import missingPhoto from '../../assets/img/missing-photo.png'
+
+interface Props extends IemployeeCard { }
 
 function EmployeeCard({
     firstName,
     lastName,
     position,
     imageSrc
-}:Props) {
-  return (
-    <div>EmployeeCard</div>
-  )
+}: Props) {
+    return (
+        <Box>
+            <Box
+                component="img"
+                src={imageSrc ? `/src/assets/img/${imageSrc}` : missingPhoto}
+                alt={`${firstName} ${lastName} Photo`}
+                sx={{
+                    width: '100%',
+                    height: {
+                        xs: "300px",
+                        sm: "400px",
+                        lg: "500px"
+                    },
+                    objectFit: 'cover'
+                }}
+            >
+            </Box>
+
+            <Box textAlign='center' color='custom.theme.darkGray' mt={2} mb={4}>
+                <Typography textTransform='uppercase' variant="h5" component='p' >{firstName} {lastName}</Typography>
+                <Typography variant="body1" component='p' >{position}</Typography>
+            </Box>
+        </Box>
+    )
 }
 
 export default EmployeeCard
