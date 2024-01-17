0de8d4d1ec0c8b3faf16d013d848434d8a1edfd9
geekydev125 <goldenstardev1108@gmail.com>
2024-01-17 07:53:29 +0200

    Adding optional padding and margin props to the CustomDivider

--- client/src/components/CustomDivider.tsx
+++ client/src/components/CustomDivider.tsx
@@ -1,10 +1,46 @@
 import Stack from '@mui/material/Stack'
 import divider from '../assets/img/divider.jpg'
 
-function CustomDivider() {
+interface Props {
+    mt?: number,
+    mb?: number,
+    my?: number,
+    ml?: number,
+    mr?: number,
+    mx?: number,
+    m?: number,
+    pt?: number,
+    pb?: number,
+    py?: number,
+    pl?: number,
+    pr?: number,
+    px?: number,
+    p?: number
+}
+function CustomDivider({
+    mt,
+    mb,
+    my,
+    ml,
+    mr,
+    mx,
+    m,
+    pt,
+    pb,
+    py,
+    pl,
+    pr,
+    px,
+    p
+}: Props) {
     return (
-        <Stack display={'flex'} justifyContent={'center'} alignItems={'center'}>
-            <img src={divider} alt='Divider' width={'auto'} height={'auto'}/>
+        <Stack
+            display={'flex'}
+            justifyContent={'center'}
+            alignItems={'center'}
+            mt={mt} mb={mb} my={my} ml={ml} mr={mr} mx={mx} m={m} pt={pt} pb={pb} py={py} pl={pl} pr={pr} px={px} p={p}
+        >
+            <img src={divider} alt='Divider' width={'auto'} height={'auto'} />
         </Stack>
     )
 }
