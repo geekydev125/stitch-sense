6c859ce5de02f6c52f7a18bc2599cfe392c40b2a
geekydev125 <goldenstardev1108@gmail.com>
2024-01-17 11:22:25 +0200

    Redesigning the SimpleCard component with new styles and content rearrangement

--- client/src/components/SimpleCard.tsx
+++ client/src/components/SimpleCard.tsx
@@ -2,28 +2,65 @@ import Box from "@mui/material/Box"
 import Card from "@mui/material/Card"
 import Stack from "@mui/material/Stack"
 import Typography from "@mui/material/Typography"
+import { Icard } from "./Sections/HomeViewSections/WhyTrustUsSection"
+import Paper from "@mui/material/Paper"
 
-interface Props {
-    title: string,
-    content: string,
-    imageSrc: string
+interface Props extends Icard {
 }
 
 function SimpleCard({
     title,
     content,
-    imageSrc
+    imageSrc,
+    variant
 }: Props) {
     return (
-        <Card sx={{ minHeight: '400px' }} elevation={0}>
-            <Stack sx={{ width: '100%', height: '150px' }} p={1} mt={2} justifyContent='center' alignItems='center'>
-                <Box component={'img'} src={imageSrc} alt='Customer Care Icon' height="100%" />
-            </Stack>
-            <Stack p={3}>
-                <Typography variant='h4' component='h4' textAlign='center'>{title}</Typography>
-                <Typography variant='body1' component='p' textAlign='justify' mt={3}>{content}</Typography>
-            </Stack>
-        </Card>
+        <Paper sx={{ borderRadius: '20px' }} elevation={6}>
+            <Card
+                sx={{
+                    minHeight: {
+                        xs: 'auto',
+                        sm: '530px',
+                        lg: '430px'
+                    },
+                    backgroundColor: variant === 'light' ? 'custom.theme.almostWhite' : 'custom.theme.darkGray',
+                    borderRadius: '20px',
+                    border: '2px solid',
+                    borderColor: variant === 'light' ? 'custom.theme.darkGray' : 'custom.theme.almostWhite',
+                }}
+                elevation={0}
+            >
+                <Box p={2}>
+                    <Typography
+                        variant='h4'
+                        component='h4'
+                        textAlign='center'
+                        textTransform='uppercase'
+                        sx={{
+                            color: variant === 'light' ? 'custom.theme.darkGray' : 'custom.theme.almostWhite',
+                        }}
+                    >
+                        {title}
+                    </Typography>
+
+                    <Stack sx={{ width: '100%', height: '120px' }} p={1} mt={2} justifyContent='center' alignItems='center'>
+                        <Box component={'img'} src={imageSrc} alt='Customer Care Icon' height="100%" />
+                    </Stack>
+
+                    <Typography
+                        variant='body1'
+                        component='p'
+                        textAlign='justify'
+                        mt={3}
+                        sx={{
+                            color: variant === 'light' ? 'custom.theme.darkGray' : 'custom.theme.almostWhite',
+                        }}
+                    >
+                        {content}
+                    </Typography>
+                </Box>
+            </Card>
+        </Paper>
     )
 }
 
