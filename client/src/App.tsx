b4d32b530ffee146abd43c471de1f04f62ba7fd7
geekydev125 <goldenstardev1108@gmail.com>
2024-01-19 06:09:07 +0200

    Adding the ServiceTabsProvider to the App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,13 +7,16 @@ import { ThemeProvider } from "@emotion/react";
 import theme from "./theme/theme";
 
 import { CssBaseline } from "@mui/material";
+import { ServiceTabsProvider } from "./contexts/ServiceTabsContext";
 
 function App() {
     return (
         <>
             <CssBaseline />
             <ThemeProvider theme={theme}>
-                <RouterProvider router={router} />
+                <ServiceTabsProvider>
+                    <RouterProvider router={router} />
+                </ServiceTabsProvider>
             </ThemeProvider>
         </>
     );
