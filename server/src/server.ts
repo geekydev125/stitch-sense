07d1c75b95cfaf740ed7fcfe6afa1f7407b8d976
geekydev125 <goldenstardev1108@gmail.com>
2024-01-21 09:03:34 +0200

    Adding cors to the server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,16 @@
 import express, { Application } from 'express'
 import logger from 'morgan/index.js'
 import router from './router/router';
+import cors from 'cors';
 
 const PORT = process.env.PORT || 3000;
 
 const app: Application = express()
 
+app.use(express.json())
 app.use(logger('dev'))
-app.use("/api", router);
+app.use(cors());
 
+app.use("/api", router);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
