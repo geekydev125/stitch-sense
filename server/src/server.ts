f47e3f3a377c3c8b52c7b7eb939382fad1e27d15
geekydev125 <goldenstardev1108@gmail.com>
2024-01-21 12:33:55 +0200

    Fixing bug on server

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 app.use(logger('dev'))
 app.use(cors({
-   origin: ['http://localhost:5173/', 'http://stitch-sense.vercel.app'],
+   origin: ['http://localhost:5173', 'http://stitch-sense.vercel.app'],
    methods: ['POST', 'GET']
 }));
 
