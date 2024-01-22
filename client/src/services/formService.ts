85a335ab9b0eeeb53f680abccbcca61d9ae6ebe2
geekydev125 <goldenstardev1108@gmail.com>
2024-01-22 08:53:33 +0200

    Fixing the baseUrl

--- client/src/services/formService.ts
+++ client/src/services/formService.ts
@@ -1,10 +1,10 @@
 import { FormData } from '../components/Sections/ContactUsView/ContactForm'
 
-const baseUrl = 'http://localhost:3000/api/contact'
+const baseUrl = 'http://localhost:3000/api'
 // const baseUrl = 'https://stitch-sense.vercel.app/api'
 // const baseUrl = 'http://stitch-sense.vercel.app/api'
 
-export const submit = (formData: FormData) => fetch(`${baseUrl}/form`, {
+export const submit = (formData: FormData) => fetch(`${baseUrl}/contact`, {
     method: 'POST',
     headers: {
         'Content-type': 'application/json'
