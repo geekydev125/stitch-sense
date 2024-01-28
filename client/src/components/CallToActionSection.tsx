f6891a4b99fae658f0fa914acd63ac98136faea4
geekydev125 <goldenstardev1108@gmail.com>
2024-01-28 18:38:11 +0200

    Changing the CallToActionSection text

--- client/src/components/CallToActionSection.tsx
+++ client/src/components/CallToActionSection.tsx
@@ -10,18 +10,18 @@ import GreenButtonWithLink from './GreenButtonWithLink'
 function CallToActionSection() {
     const theme = useTheme();
     const isXs = useMediaQuery(theme.breakpoints.down('sm'));
-    
+
     return (
 
         <Box px={{ xs: 2, md: 10 }}>
-            <Typography variant="body1" component="p" textAlign={{ xs: 'justify', md: 'center'}} color='custom.theme.darkGray'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim.
+            <Typography variant="body1" component="p" textAlign={{ xs: 'justify', md: 'center' }} color='custom.theme.darkGray'>
+                Elevate your fashion brand with StitchSense – revolutionize your designs, streamline your production, and bring your vision to life. Take the next step in crafting excellence, and experience the power of StitchSense for a seamless journey in the world of fashion.
             </Typography>
 
             <Stack my={5} direction='row' justifyContent={'space-around'} alignItems='center'>
-               {!isXs && <Arrow arrowType={1} scaleX={1} rotate={0} height={20} />} 
+                {!isXs && <Arrow arrowType={1} scaleX={1} rotate={0} height={20} />}
                 <GreenButtonWithLink boxShadow variant='contained' size='large' linkTo='contact-us'> LET'S GET STARTED</GreenButtonWithLink>
-                <Arrow arrowType={1} height={20} scaleX={-1} rotate={0}/>
+                <Arrow arrowType={1} height={20} scaleX={-1} rotate={0} />
             </Stack>
         </Box>
     )
