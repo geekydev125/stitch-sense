fc56df8615f4d6110da743e73bfae9aca979890b
geekydev125 <goldenstardev1108@gmail.com>
2024-01-28 10:36:10 +0200

    Updating util to return a result without defining a folder

--- client/src/utils/image-util.ts
+++ client/src/utils/image-util.ts
@@ -1,5 +1,9 @@
-function getImageUrl(imageFolder:string, imageName: string) {
+function getImageUrl(imageFolder:string | null | undefined, imageName: string) {
+  if(!imageFolder){
+    return new URL(`../assets/img/${imageName}`, import.meta.url).href;
+  }
   return new URL(`../assets/img/${imageFolder}/${imageName}`, import.meta.url).href;
+  
 }
 
 export default getImageUrl;
