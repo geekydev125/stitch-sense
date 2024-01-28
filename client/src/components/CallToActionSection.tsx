0cecd3baa8f27191c7bfbaa8afbe8dbd8fec266d
geekydev125 <goldenstardev1108@gmail.com>
2024-01-28 17:43:56 +0200

    Adding a link to the button in the section

--- client/src/components/CallToActionSection.tsx
+++ client/src/components/CallToActionSection.tsx
@@ -1,11 +1,11 @@
 import Arrow from './Arrow'
-import GreenButton from './GreenButton'
 
 import Box from '@mui/material/Box'
 import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
 import { useTheme } from '@mui/material/styles'
 import useMediaQuery from '@mui/material/useMediaQuery'
+import GreenButtonWithLink from './GreenButtonWithLink'
 
 function CallToActionSection() {
     const theme = useTheme();
@@ -20,7 +20,7 @@ function CallToActionSection() {
 
             <Stack my={5} direction='row' justifyContent={'space-around'} alignItems='center'>
                {!isXs && <Arrow arrowType={1} scaleX={1} rotate={0} height={20} />} 
-                <GreenButton boxShadow variant='contained' size='large'> LET'S GET STARTED</GreenButton>
+                <GreenButtonWithLink boxShadow variant='contained' size='large' linkTo='contact-us'> LET'S GET STARTED</GreenButtonWithLink>
                 <Arrow arrowType={1} height={20} scaleX={-1} rotate={0}/>
             </Stack>
         </Box>
