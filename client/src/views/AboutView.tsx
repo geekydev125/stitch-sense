e30d11f3e12152da3431786ebc2ab4e0c73d8fc4
geekydev125 <goldenstardev1108@gmail.com>
2024-01-28 18:39:58 +0200

    Adding the content from the WhoWeAreSection to the AboutView

--- client/src/views/AboutView.tsx
+++ client/src/views/AboutView.tsx
@@ -18,6 +18,7 @@ import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
 import CallToActionSection from '../components/CallToActionSection'
 import { Icard } from '../components/Sections/HomeView/WhyTrustUsSection'
+import SectionTitle from '../components/SectionTitle'
 
 export interface IEmployee {
 	firstName: string
@@ -28,28 +29,39 @@ export interface IEmployee {
 
 function AboutView() {
 	return (
-		<Box mt={5} mb={{xs: 3, md: 8}}>
+		<Box mt={5} mb={{ xs: 3, md: 8 }}>
 			<Container>
-				<Typography variant="body1" component="p" textAlign={{xs: 'justify', md: 'center'}} color='custom.theme.darkGray'>
-					Lorem ipsum dolor sit amet, an eum habeo explarasdi. Et sed exerci hendrerit, no summo simul partiendo est. Modus iriure voluptua at pri, cu copiosae appetere electram mei. Solum dolorum tractatos mea at. Eu commodo gubergren mel, in vix alterum dolores laboramus.
-				</Typography>
+				<Box p={5}>
+					<Typography variant='body1' component='p' textAlign='justify' color='custom.theme.darkGray'>
+						With <Box component='span' textTransform='uppercase' fontWeight='bold'>over a decade of expertise in the dynamic realm of fashion</Box>, we proudly stand as a seasoned clothing manufacturer. Our commitment to excellence has propelled us into various facets of the industry.
+					</Typography>
+					<br />
+					<Typography variant='body1' component='p' textAlign='justify' color='custom.theme.darkGray'>
+						Functioning as a premier white-label provider, our clientele spans diverse retail landscapes—from mass and specialty outlets to department stores, off-price retailers, and cutting-edge e-commerce platforms. Our core focus revolves around ensuring rapid market delivery and adopting direct-to-consumer strategies.
+					</Typography>
+					<br />
+					<Typography variant='body1' component='p' pb={2} textAlign='justify' color='custom.theme.darkGray'>
+						Based in Europe, we are the creative force behind renowned clothing brands such as <Box component='span' fontWeight='bold'>Badinka, CandyCatz and RaveKitties</Box>. For more than 10 years, these brands have curated captivating fashion experiences for our delighted customers, showcasing our unwavering dedication to style and quality.
+					</Typography>
+				</Box>
 
 
-				<CustomDivider mt={{xs: 3, md: 4}} mb={{xs: 2, md: 4}} />
-
-				<Grid container spacing={{xs: 1, md: 2}} >
+				{/* <CustomDivider  mb={{ xs: 2, md: 4 }} /> */}
+				<SectionTitle title='OUR IMPACT ON EMPLOYEES' variant='h5' justify='left' p={2} mb={2}/>
+				<Grid container spacing={{ xs: 1, md: 2 }} >
 					{employeesImpactCardsContent.map(card => {
 						return (
 							<Grid key={uniqid()} item xs={12} md={4}>
-								 <SimpleEmployeeCard {...card as Omit<Icard, "imageSrc" | "alt">} />
+								<SimpleEmployeeCard {...card as Omit<Icard, "imageSrc" | "alt">} />
 							</Grid>
 						)
 					})}
 				</Grid>
 
-				<CustomDivider my={{xs: 3, md: 5}} />
+				<CustomDivider mt={{ xs: 3, md: 6 }} mb={{ xs: 3, md: 3 }} />
 
-				<Grid container spacing={{xs: 1, md: 2}}>
+				<SectionTitle title='OUR TEAM' variant='h5' justify='left' p={2} mb={2}/>
+				<Grid container spacing={{ xs: 1, md: 2 }}>
 					{employeesData.map((employee: IEmployee) => (
 						<Grid key={uniqid()} item xs={6} md={4}>
 							<EmployeeCard {...employee} />
@@ -58,8 +70,8 @@ function AboutView() {
 				</Grid>
 
 
-				<CustomDivider mt={{xs: 1, md: 3}} mb={{xs: 4, md: 5}} />
-				
+				<CustomDivider mt={{ xs: 1, md: 3 }} mb={{ xs: 4, md: 5 }} />
+
 				<CallToActionSection />
 			</Container>
 		</Box>
