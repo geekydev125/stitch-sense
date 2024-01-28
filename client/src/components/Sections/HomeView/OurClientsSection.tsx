2d3655c21ed56b288262f85ac12d1f0e665143de
geekydev125 <goldenstardev1108@gmail.com>
2024-01-28 14:01:21 +0200

    Redesigning the OurClients section

--- client/src/components/Sections/HomeView/OurClientsSection.tsx
+++ client/src/components/Sections/HomeView/OurClientsSection.tsx
@@ -1,46 +1,36 @@
+import uniqid from 'uniqid'
+
+import getImageUrl from '../../../utils/image-util'
+
 import clients from '../../../data/clients.json'
 
 import Box from "@mui/material/Box"
 import Container from "@mui/material/Container"
 import Grid from "@mui/material/Grid"
-import Stack from "@mui/material/Stack"
 import Typography from "@mui/material/Typography"
-import uniqid from 'uniqid'
-import SectionTitle from '../../SectionTitle'
-import getImageUrl from '../../../utils/image-util'
+import Divider from '@mui/material/Divider'
 
 function OurClientsSection() {
     return (
-        <Box component='section'>
+        <Box component='section' sx={{ backgroundColor: 'black', width: '100%', minHeight: 'auto', maxHeight: '100%' }} mt={2} py={{ md: 3 }}>
             <Container>
-                <Grid container spacing={1}>
-                    <Grid item xs={12} md={4}>
-                        <Stack sx={{ height: '100%' }} justifyContent='center' alignItems='center'>
-                            <SectionTitle title='OUR CLIENTS' p={2} />
-                        </Stack>
-                    </Grid>
-                    <Grid item xs={12} md={1}>
+                <Box pt={1} pb={5}>
+                    <Typography my={1} mb={3} variant="h3" component='p' textAlign='center' color='custom.theme.almostWhite' fontWeight='bold'>OUR CLIENTS</Typography>
 
-                    </Grid>
-                    <Grid item xs={12} md={7}>
-                        <Typography variant='body1' component='p' textAlign={{xs: 'justify', md: 'center'}} color='custom.theme.darkGray'>We have years of experience in the fashion industry with highly successful clothing brands like <b>CandyCatz</b>, <b>Badinka</b>, <b>RaveKitty</b> and <b>Super 5 Clothing</b> – all dedicated to delivering top-notch quality and style.</Typography>
-                    </Grid>
-                </Grid>
-            </Container>
+                    <Divider variant="middle" color='white' />
 
-            <Box sx={{ backgroundColor: 'black', width: '100%', minHeight: 'auto', maxHeight: '100%' }} mt={2} py={{md: 3}}>
-                <Container>
                     <Grid container >
                         {clients.map(client => {
                             return (
                                 <Grid key={uniqid()} item xs={6} md={3} display='flex' justifyContent='center' alignItems='center'>
-                                    <Box component='img' src={getImageUrl('logos',client.imageSrc)} alt={`${client.title} Logo`} maxWidth='100%' />
+                                    <Box component='img' src={getImageUrl('logos', client.imageSrc)} alt={`${client.title} Logo`} maxWidth='100%' />
                                 </Grid>
                             )
                         })}
                     </Grid>
-                </Container>
-            </Box>
+                    <Divider variant="middle" color='white' />
+                </Box>
+            </Container>
         </Box>
     )
 }
