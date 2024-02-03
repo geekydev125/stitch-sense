517cb7f8fe4862f8c5aee5e034693facf6d81b88
geekydev125 <goldenstardev1108@gmail.com>
2024-02-03 11:20:15 +0200

    Creating a cors-config file

---
+++ server/src/config/cors-config.ts
@@ -0,0 +1,9 @@
+let allowedOrigins: string[] | [];
+
+if (process.env.NODE_ENV === 'development') {
+    allowedOrigins = ['http://localhost:5173', 'http://localhost:3000/', 'http://stitch-sense.vercel.app'];
+} else {
+    allowedOrigins = [];
+}
+
+export default allowedOrigins;
