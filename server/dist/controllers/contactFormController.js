5bdecc2f1eb475c2152bccd9d461bc2631222059
geekydev125 <goldenstardev1108@gmail.com>
2024-02-03 20:04:05 +0200

    including server/dist in repo

---
+++ server/dist/controllers/contactFormController.js
@@ -0,0 +1,69 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const nodemailer_1 = __importDefault(require("nodemailer"));
+const router = (0, express_1.Router)();
+router.post('/', async (req, res) => {
+    let { firstName, lastName, phone, email, contactMethod, message } = req.body;
+    switch (contactMethod) {
+        case 'phone':
+            contactMethod = 'Phone';
+            break;
+        case 'email':
+            contactMethod = 'Email';
+            break;
+        case 'phoneAndEmail':
+            contactMethod = 'Phone AND/OR Email';
+            break;
+        default:
+            return contactMethod;
+    }
+    const output = `
+    <h2>You have a new form submission on Stitch Sense!</h2>
+    <h3>Submission Details</h3>
+    <ul>
+        <li><b>Name:</b> ${firstName} ${lastName}</li>
+        <li><b>Phone number:</b> ${phone}</li>
+        <li><b>Email address:</b> ${email}</li>
+        <li><b>Preferred contact method:</b> ${contactMethod}</li>
+        <li><b>Message:</b> ${message}</li>
+    </ul>
+`;
+    const transporter = nodemailer_1.default.createTransport({
+        service: 'hotmail',
+        host: 'smtp.live.com',
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.EMAIL_FROM,
+            pass: process.env.PASSWORD,
+        },
+        from: process.env.EMAIL_FROM,
+        tls: {
+            rejectUnauthorized: false,
+        },
+    });
+    const options = {
+        from: `Stitch Sense <${process.env.EMAIL_FROM}>`,
+        to: process.env.EMAIL_TO,
+        subject: 'Stitch Sense form submission',
+        text: 'Stitch Sense form submission',
+        html: output,
+    };
+    const info = await transporter.sendMail(options);
+    transporter.verify(function (err, success) {
+        if (err) {
+            res.status(500).send({ message: 'An error occurred while attempting to process your message.' });
+            console.log(err);
+        }
+        else {
+            console.log('Message sent: %s', info.messageId);
+            res.status(200).json({ success: success });
+        }
+    });
+});
+exports.default = router;
+//# sourceMappingURL=contactFormController.js.map
---
+++ server/dist/public/assets/RootView-IQ9etSJQ.js
@@ -0,0 +1,31 @@
+import{s as o,j as n,S as s,a as i,B as e,u as a,b as r,H as c,r as d,O as l,F as x}from"./index-9FoZLJz1.js";const u=o("div")`
+	.bouncing-loader{
+		display: flex;
+		justify-content: center;
+	}
+
+	.bouncing-loader > div {
+		width: 16px;
+		height: 16px;
+		margin: 3px 6px;
+		border-radius: 50%;
+		background-color: #ffffff;
+		opacity: 1;
+		animation: bouncing-loader 0.6s infinite alternate;
+	}
+
+	@keyframes bouncing-loader {
+		to {
+			opacity: 0.1;
+			transform: translateY(-16px);
+		}
+	}
+
+	.bouncing-loader > div:nth-of-type(2) {
+		animation-delay: 0.2s;
+	}
+
+	.bouncing-loader > div:nth-of-type(3) {
+		animation-delay: 0.4s;
+	}
+`;function h(){return n.jsx(u,{children:n.jsxs("div",{className:"bouncing-loader",children:[n.jsx("div",{}),n.jsx("div",{}),n.jsx("div",{})]})})}function j(){return n.jsxs(s,{justifyContent:"center",alignItems:"center",zIndex:10,sx:{position:"absolute",width:"100vw",height:"100vh",backgroundColor:"custom.theme.lightGreen"},children:[n.jsx("img",{src:i,alt:"Stitch Sense Logo"}),n.jsx(e,{mt:5,children:n.jsx(h,{})})]})}function g(){const t=a().pathname==="/";return n.jsxs(n.Fragment,{children:[n.jsx(r,{}),!t&&n.jsx(c,{}),n.jsx(d.Suspense,{fallback:n.jsx(j,{}),children:n.jsx(e,{component:"main",children:n.jsx(l,{})})}),n.jsx(x,{})]})}export{g as default};
