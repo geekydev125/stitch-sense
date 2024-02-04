1251f37fc8a2d76ede9a066b8e8962b0f6690342
geekydev125 <goldenstardev1108@gmail.com>
2024-02-04 14:05:26 +0200

    Creating a separate base-url file

---
+++ client/src/config/base-url.ts
@@ -0,0 +1,5 @@
+const baseUrl = "http://localhost:3000/api"
+// const baseUrl = "https://stitch-sense.vercel.app/api"
+// const baseUrl = "https://stitch-sense.onrender.com/api"
+
+export default baseUrl;
