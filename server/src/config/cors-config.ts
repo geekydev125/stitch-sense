fafec51e6dc2a935e76cb9fbfba636e462fd6d8e
geekydev125 <goldenstardev1108@gmail.com>
2024-02-04 08:18:14 +0200

    Updating cors-config of vercel url to be https

--- server/src/config/cors-config.ts
+++ server/src/config/cors-config.ts
@@ -1,7 +1,7 @@
 let allowedOrigins: string[] | [];
 
 if (process.env.NODE_ENV === 'development') {
-    allowedOrigins = ['http://localhost:5173', 'http://localhost:3000/', 'http://stitch-sense.vercel.app'];
+    allowedOrigins = ['http://localhost:5173', 'http://localhost:3000/', 'https://stitch-sense.vercel.app'];
 } else {
     allowedOrigins = ['https://stitch-sense.onrender.com'];
 }
