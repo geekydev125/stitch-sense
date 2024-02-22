a7bee1b2ad431e1efc3aa3c7b5436cbd29514288
geekydev125 <goldenstardev1108@gmail.com>
2024-02-22 19:07:38 +0200

    Adding a new build for deployment

---
+++ server/src/public/assets/GreenButtonWithLink-EjxFYhMg.js
@@ -0,0 +1,1 @@
+import{j as t,L as x}from"./index-PzNd3gUt.js";import{G as u}from"./GreenButton-B6_Ns5lK.js";function p({type:e,disabled:n,variant:o="text",children:r,boxShadow:i,size:s="medium",linkTo:m}){return t.jsx(x,{to:`/${m}`,style:{textDecoration:"none"},children:t.jsx(u,{type:e,variant:o,disabled:n,size:s,boxShadow:i,children:r})})}export{p as G};
