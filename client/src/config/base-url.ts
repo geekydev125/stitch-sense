136dd7f03c74ec664a55135adadf5fe49ded102b
geekydev125 <goldenstardev1108@gmail.com>
2024-02-23 12:48:43 +0200

    Updating baseUrl to actual stitch-sense site

--- client/src/config/base-url.ts
+++ client/src/config/base-url.ts
@@ -1,4 +1,5 @@
 // const baseUrl = "http://localhost:3000/api"
-const baseUrl = "https://stitch-sense.onrender.com/api"
+// const baseUrl = "https://stitch-sense.onrender.com/api"
+const baseUrl = "https://stitch-sense.com/api"
 
 export default baseUrl;
