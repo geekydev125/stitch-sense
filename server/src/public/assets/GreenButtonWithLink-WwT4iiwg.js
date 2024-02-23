a855490ae8f98dea243f98dc0e3bb152c8df1f9a
geekydev125 <goldenstardev1108@gmail.com>
2024-02-23 13:00:17 +0200

    Updating baseUrl by adding "www." and adding a new build for production

--- server/src/public/assets/GreenButtonWithLink-WwT4iiwg.js
+++
@@ -1,1 +0,0 @@
-import{j as t,L as x}from"./index-PUloDbvA.js";import{G as u}from"./GreenButton-XMsSbn_2.js";function p({type:e,disabled:n,variant:o="text",children:r,boxShadow:i,size:s="medium",linkTo:m}){return t.jsx(x,{to:`/${m}`,style:{textDecoration:"none"},children:t.jsx(u,{type:e,variant:o,disabled:n,size:s,boxShadow:i,children:r})})}export{p as G};
