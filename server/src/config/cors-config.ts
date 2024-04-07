841820bbd7655e19f470411df01c0097f782d6c7
geekydev125 <goldenstardev1108@gmail.com>
2024-04-07 09:03:21 +0300

    Updating the allowed cors origins

--- server/src/config/cors-config.ts
+++ server/src/config/cors-config.ts
@@ -6,7 +6,7 @@ let allowedOrigins: string[] | [];
 if (process.env.NODE_ENV === 'development') {
     allowedOrigins = ['http://localhost:5173', 'http://localhost:3000/', 'https://stitch-sense.vercel.app'];
 } else {
-    allowedOrigins = ['https://www.stitch-sense.com/', 'https://stitch-sense.onrender.com'];
+    allowedOrigins = ['https://www.stitch-sense.com/', 'https://stitch-sense.onrender.com', "https://stitch-sense.vercel.app/"];
 }
 
 export default allowedOrigins;
