6e13a0fde24a0c574895869ee64dc8cc794ade5d
geekydev125 <goldenstardev1108@gmail.com>
2024-07-04 23:18:01 +0300

    Creating a Divider component

---
+++ components/Common/Divider.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image"
+
+function Divider() {
+    return (
+        <div className="flex justify-center my-3 md:my-7">
+            <Image
+                className="transform scale-50 md:scale-100"
+                src="/assets/img/arrows-divider/divider.png"
+                alt="Divider"
+                width={135}
+                height={30}
+            />
+        </div>
+    )
+}
+
+export default Divider
