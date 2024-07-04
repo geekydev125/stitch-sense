b5f1f2ea51813cbba5cc4cd2d06f9c48c9b90429
geekydev125 <goldenstardev1108@gmail.com>
2024-07-05 00:24:26 +0300

    Implementing the WhyChooseUsSection

--- components/Pages/Home/Sections/WhyChooseUs/WhyChooseUsSection.tsx
+++ components/Pages/Home/Sections/WhyChooseUs/WhyChooseUsSection.tsx
@@ -1,7 +1,22 @@
+import ParallaxWrapper from "./ParallaxWrapper"
+
+
 function WhyChooseUsSection() {
-  return (
-    <div>WhyChooseUsSection</div>
-  )
+    return (
+        <section>
+            <ParallaxWrapper>
+                <div className="container py-10">
+                    <hr className="border-white my-10" />
+                    <h3 className="text-center text-light text-7xl uppercase font-bold">Why Choose Us</h3>
+
+                    <p className="mt-5 text-2xl text-light text-center"><span className="uppercase font-bold">Our mission</span> is to foster the growth of your business. By seamlessly managing all manufacturing processes, we free up your time to nurture and promote your brand. Your success is our utmost priority, and we are dedicated to meeting your needs by delivering products of the highest quality, always on time.</p>
+                    <hr className="border-white my-10" />
+
+                </div>
+
+            </ParallaxWrapper>
+        </section>
+    )
 }
 
 export default WhyChooseUsSection
