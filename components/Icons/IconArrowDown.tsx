06e3eb14f8cc246e62cba4ad46400879ef12ac63
geekydev125 <goldenstardev1108@gmail.com>
2024-07-04 21:57:41 +0300

    Creating an IconArrowDown component

--- components/Icons/IconArrowDown.tsx
+++ components/Icons/IconArrowDown.tsx
@@ -1,14 +1,14 @@
 import { IIconProps } from '@/types/types'
 
-function IconHamburger({
+function IconArrowDown({
     sizeClassName = 'size-5'
 }:IIconProps) {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={sizeClassName}>
-            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            <path strokeLinecap="round" strokeLinejoin="round" d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
         </svg>
 
     )
 }
 
-export default IconHamburger
+export default IconArrowDown
