944a973984efc17838efb42c6564bd5d2d73b7e1
geekydev125 <goldenstardev1108@gmail.com>
2024-07-05 00:25:18 +0300

    Adjusting the z-index of the header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -3,7 +3,7 @@ import MobileMenu from "./MobileMenu"
 
 function Header() {
     return (
-        <header className="navbar sticky top-0 bg-primary text-white shadow-xl ">
+        <header className="navbar sticky top-0 bg-primary text-white shadow-xl z-10">
             <DesktopMenu />
 
             <MobileMenu />
