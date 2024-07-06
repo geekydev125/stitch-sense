061e49ba9d28953f5bb2c1432ca694c50e115821
geekydev125 <goldenstardev1108@gmail.com>
2024-07-06 15:04:05 +0300

    Further ajdusting the structure of the TopFooter

--- components/Map/LoadingMap.tsx
+++ components/Map/LoadingMap.tsx
@@ -4,10 +4,10 @@ import React from 'react'
 function LoadingMap() {
     return (
         <div className='h-full flex flex-row justify-center items-center'>
-            <div className='flex items-end gap-2'>
+            <div className='flex items-center gap-2'>
                 
-                <p className='text-white text-2xl'>Loading Map...</p>
-                <Image src="/assets/img/logos/stitchSense-google-map-marker.png" alt="Stitch Sense Google Map Marker" width={40} height={40} />
+                <p className='text-white text-4xl'>Loading Map...</p>
+                <Image src="/assets/img/logos/stitchSense-google-map-marker.png" alt="Stitch Sense Google Map Marker" width={50} height={50} />
             </div>
         </div>
     )
