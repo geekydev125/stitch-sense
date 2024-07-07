77785d7da18b75a588e02b0ca05b7dfbb27888e8
geekydev125 <goldenstardev1108@gmail.com>
2024-07-07 22:42:49 +0300

    Creating the TeamCard component

---
+++ components/Cards/TeamCard/TeamCard.tsx
@@ -0,0 +1,24 @@
+import { ITeamMember } from "@/types/types"
+
+interface Props {
+    teamMember: ITeamMember
+}
+
+function TeamCard({
+    teamMember
+}: Props) {
+    return (
+        <div className="flex flex-col items-center">
+            <img src={`/assets/img/team/${teamMember.image}`} alt={`${teamMember.firstName} ${teamMember.lastName}`} />
+
+            <div className="mt-5">
+                <h3 className="text-3xl text-center ">{teamMember.firstName} {teamMember.lastName}</h3>
+
+                <p className="text-center">{teamMember.position}</p>
+            </div>
+
+        </div>
+    )
+}
+
+export default TeamCard
