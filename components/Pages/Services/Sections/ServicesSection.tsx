97b33c2fd94a20b2c8460a71a8f8be939d6186d6
geekydev125 <goldenstardev1108@gmail.com>
2024-07-07 17:24:58 +0300

    Creating the ServicesSection and adding the Tabs into it

---
+++ components/Pages/Services/Sections/ServicesSection.tsx
@@ -0,0 +1,15 @@
+import Divider from "@/components/Common/Divider"
+import Tabs from "@/components/Tabs/Tabs"
+
+
+function ServicesSection() {
+    return (
+        <>
+            <Tabs />
+
+            <Divider />
+        </>
+    )
+}
+
+export default ServicesSection
