9d2514ca68f992f7ef80d04f431cf1e0d69b4b88
geekydev125 <goldenstardev1108@gmail.com>
2024-07-13 08:37:56 +0300

    Creating the InputField component

---
+++ components/Forms/InputField.tsx
@@ -0,0 +1,22 @@
+import { UseFormRegister } from "react-hook-form"
+import { IContactFormData } from "./ContactForm"
+
+interface Props {
+    name: IContactFormData[keyof IContactFormData]
+    register: UseFormRegister<IContactFormData>
+    required?: boolean,
+    placeholder?: string
+}
+
+function InputField({
+    name,
+    register,
+    required = false,
+    placeholder = ""
+}: Props) {
+    return (
+        <input required={required} placeholder={placeholder} {...register(name as keyof IContactFormData)} type="text" className="input input-bordered w-full" />
+    )
+}
+
+export default InputField
