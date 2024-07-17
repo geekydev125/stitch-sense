6f83424703b4e5e1d37124f39a8ace4bab82f7b5
geekydev125 <goldenstardev1108@gmail.com>
2024-07-17 18:01:32 +0300

    Restructuring the TabContent component and optimizing it for mobile

--- components/Tabs/TabContent.tsx
+++ components/Tabs/TabContent.tsx
@@ -12,9 +12,10 @@ function TabContent({
     service
 }: Props) {
     return (
-        <div role="tabpanel" className=" p-10 pb-4 min-h-[420px] px-4">
-            <div className='grid grid-cols-12 gap-10 pb-0'>
-                <div className='col-span-12 md:col-span-7 flex flex-col items-center justify-between'>
+        <div role="tabpanel" className=" p-10 pb-4 min-h-[420px] px-0 md:px-4">
+            <div className='grid grid-cols-12 gap-y-5 md:gap-10 pb-0'>
+
+                <div className='col-span-12 md:col-span-7 flex flex-col items-center justify-between order-2 md:order-1'>
                     <div>
                         {
                             service.content.map((paragraph) => {
@@ -27,8 +28,15 @@ function TabContent({
 
                     <Button href='/contact' className="mt-5">Get a Quote</Button>
                 </div>
-                <div className='col-span-12 md:col-span-5 relative h-[420px] w-[420px]'>
-                    <Image src={`/assets/img/services/${service.image}`} alt={service.title} fill />
+                <div className='col-span-12 md:col-span-5 flex justify-center order-1 md:order-2'>
+                    <div className='relative 
+                    h-[170px] w-full 
+                    sm:h-[170px] sm:w-3/4 
+                    md:h-[300px] md:w-[300px]
+                    lg:h-[420px] lg:w-[420px]
+                    '>
+                        <Image src={`/assets/img/services/${service.image}`} alt={service.title} fill className='object-cover'/>
+                    </div>
                 </div>
             </div>
         </div>
