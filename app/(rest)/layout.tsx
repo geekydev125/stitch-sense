320d94f2bca682b65d5cbb6436cb9abbe5c5923e
geekydev125 <goldenstardev1108@gmail.com>
2024-07-17 14:13:38 +0300

    Implementing NextTopLoader in both root layouts

--- app/(rest)/layout.tsx
+++ app/(rest)/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
+import NextTopLoader from 'nextjs-toploader';
 import "../globals.css";
 
 import Header from "@/components/Header/Header";
@@ -23,6 +24,16 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={groteskFont.className}>
+				<NextTopLoader
+					color="#35908d"
+					height={3}
+					initialPosition={0.08}
+					crawlSpeed={200}
+					crawl={true}
+					showSpinner={false}
+					easing="ease"
+					speed={200}
+				/>
 				<Header />
 
 				<main className="pb-5 md:pb-10">
