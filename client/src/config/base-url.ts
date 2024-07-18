e567221a85fa135d9fa0434d4b6cd29dce9f8a49
geekydev125 <goldenstardev1108@gmail.com>
2024-07-18 16:51:21 +0300

    Removing unnecessary urls from base-url file

--- client/src/config/base-url.ts
+++ client/src/config/base-url.ts
@@ -1,6 +1,4 @@
 // const baseUrl = "http://localhost:3000/api"
-// const baseUrl = "https://stitch-sense.onrender.com/api"
-// const baseUrl = "https://stitch-sense.vercel.app/api"
 const baseUrl = "https://www.stitch-sense.com/api"
 
 export default baseUrl;
