ca0492912ae1d4e85d4bf888160ef0920ef08beb
geekydev125 <goldenstardev1108@gmail.com>
2024-07-18 16:50:39 +0300

    Removing unnecessary origins from cors config

--- server/src/config/cors-config.ts
+++ server/src/config/cors-config.ts
@@ -4,9 +4,9 @@ env.config();
 let allowedOrigins: string[] | [];
 
 if (process.env.NODE_ENV === 'development') {
-    allowedOrigins = ['http://localhost:5173', 'http://localhost:3000/', 'https://stitch-sense.vercel.app'];
+    allowedOrigins = ['http://localhost:5173', 'http://localhost:3000/'];
 } else {
-    allowedOrigins = ['https://www.stitch-sense.com/', 'https://stitch-sense.onrender.com', "https://stitch-sense.vercel.app/"];
+    allowedOrigins = ['https://www.stitch-sense.com/'];
 }
 
 export default allowedOrigins;
