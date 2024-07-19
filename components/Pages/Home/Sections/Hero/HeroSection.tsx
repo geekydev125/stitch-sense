50f01f8a4b555ef013cfe96cbc269b614793fa99
geekydev125 <goldenstardev1108@gmail.com>
2024-07-19 21:26:10 +0300

    Optimizing the HeroSection for mobile

--- components/Pages/Home/Sections/Hero/HeroSection.tsx
+++ components/Pages/Home/Sections/Hero/HeroSection.tsx
@@ -13,14 +13,19 @@ function HeroSection() {
             <BackgroundVideoMask>
                 <div className='container h-full'>
                     <div className='flex flex-col justify-center items-start h-full'>
-
                         <div className='flex flex-col items-center w-full md:w-auto'>
-                            <h1 className='text-5xl md:text-8xl mb-5 text-light md:text-dark'>StitchSense</h1>
-                            <p className='uppercase text-lg md:text-xl font-bold text-center mb-5 text-light md:text-dark'>Where your business production needs are met</p>
-                            <Image className='md:hidden' src='/assets/img/logos/stitchSense-logo-white.png' width={65} height={65} alt='Arrow down' />
-                            <Image className='hidden md:block' src='/assets/img/logos/stitchSense-logo-black.png' width={150} height={150} alt='Arrow down' />
-                        </div>
+                            <h1 className='text-5xl sm:text-8xl md:text-7xl lg:text-[103px] lg:leading-[100px] mb-4 text-light md:text-dark'>StitchSense</h1>
+                            <p className='uppercase px-10 sm:px-0 sm:text-xl md:text-lg lg:text-[23px] text-center mb-6 text-light md:text-dark'>Where your business production needs are met</p>
 
+                            <div className='relative md:hidden w-[65px] h-[65px] sm:w-[95px] sm:h-[95px]' >
+                                <Image src='/assets/img/logos/stitchSense-logo-white.png' fill alt='Arrow down' />
+                            </div>
+
+                            <div className='relative hidden md:block w-[120px] h-[120px] lg:w-[130px] lg:h-[130px]' >
+                                <Image src='/assets/img/logos/stitchSense-logo-black.png' fill alt='Arrow down' />
+                            </div>
+
+                        </div>
                     </div>
                 </div>
             </BackgroundVideoMask>
