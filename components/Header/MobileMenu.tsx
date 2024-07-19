00081f2fb07ee9a4b7d0959fa503360fbe995dbd
geekydev125 <goldenstardev1108@gmail.com>
2024-07-19 19:02:01 +0300

    Implementing the MobileMenuDrawer in the MobileMenu

--- components/Header/MobileMenu.tsx
+++ components/Header/MobileMenu.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image"
 import Link from "next/link"
 
-import IconHamburger from "../Icons/IconHamburger"
+import MobileMenuDrawer from "./MobileMenuDrawer"
 
 function MobileMenu() {
     return (
         <div className="md:hidden px-4 flex flex-row justify-between w-full">
-            <IconHamburger sizeClassName="size-7" />
+            <MobileMenuDrawer />
 
             <Link href="/">
                 <Image src="/assets/img/logos/stitchSense-logo-white.png" alt='Stitch Sense Logo' width={50} height={50} />
