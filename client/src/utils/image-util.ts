02202d80ba467528afca939cf79235f93aa69fdd
geekydev125 <goldenstardev1108@gmail.com>
2024-07-23 00:11:19 +0300

    Deleting all files from main branch

--- client/src/utils/image-util.ts
+++
@@ -1,9 +0,0 @@
-function getImageUrl(imageFolder:string | null | undefined, imageName: string) {
-  if(!imageFolder){
-    return new URL(`../assets/img/${imageName}`, import.meta.url).href;
-  }
-  return new URL(`../assets/img/${imageFolder}/${imageName}`, import.meta.url).href;
-  
-}
-
-export default getImageUrl;
--- client/src/utils/regex.ts
+++
@@ -1,4 +0,0 @@
-//Valid Phone number pattern:
-export const PHONE_PATTERN_REGEX = /^(\+?\d{0,4})?\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{4}\)?)?$/
-
-export const LATIN_CHARACTERS_REGEX = /[a-zA-z]/i
